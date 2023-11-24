8c96cdce7f92e3c24fbf595e5cd6909b630e9978
Ibrahim Aji <ibrahim.aji@live.com>
2023-11-24 09:01:33 +0700

    feat: add counter

---
+++ src/components/CounterPage.tsx
@@ -0,0 +1,24 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Counter } from "./Counter"
+
+
+export const CounterPage = () => {
+    return (
+        <Card>
+            <CardHeader className="flex items-center justify-center">
+                <CardTitle>Counter App</CardTitle>
+                <CardDescription>Just a simple counter app</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Counter initialValue={5}></Counter>
+            </CardContent>
+        </Card>
+    )
+}
